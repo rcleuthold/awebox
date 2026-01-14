006ef2c8fc64ba226ae76bb4feac364cdcc1ee90
RCLeuthold <rcleuthold@gmail.com>
2026-01-14 15:38:11 +0100

    [UNTESTED] run veri_dyn 3 times

---
+++ examples/Leuthold_2025_RLL_paper_scripts/with_ineq_at_all_but_integral_and_tf_fraction/veri_dyn2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --partition=highmem
+#SBATCH --job-name=veri_dyn_main2
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=96
+#SBATCH --mem=600G
+#SBATCH --output=veri_dyn_main2.out
+#SBATCH --error=veri_dyn_main2.err
+
+echo "veri dyn main 2!"
+export PYTHONPATH="$HOME/awebox:${PYTHONPATH:-}"
+
+set -euo pipefail
+
+module purge
+module load devel/miniforge   # bwHPC module :contentReference[oaicite:1]{index=1}
+
+# Make "conda activate" available in this non-interactive shell:
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate ocp
+
+python /pfs/data6/home/fr/fr_fr/fr_rl1038/awebox/examples/Leuthold_2025_RLL_paper_scripts/with_ineq_at_all_but_integral_and_tf_fraction/verification_dynamic_conditions_from_haas2019.py
