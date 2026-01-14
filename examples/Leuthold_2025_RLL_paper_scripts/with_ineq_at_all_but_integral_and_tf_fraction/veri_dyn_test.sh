7e6792fa1f01edc25b62cfcdcff5f1bde9bb635c
RCLeuthold <rcleuthold@gmail.com>
2026-01-14 15:00:41 +0100

    [UNTESTED] fix typo in veri-test shell scripts

--- examples/Leuthold_2025_RLL_paper_scripts/with_ineq_at_all_but_integral_and_tf_fraction/veri_dyn_test.sh
+++ examples/Leuthold_2025_RLL_paper_scripts/with_ineq_at_all_but_integral_and_tf_fraction/veri_dyn_test.sh
@@ -20,6 +20,7 @@ module load devel/miniforge   # bwHPC module :contentReference[oaicite:1]{index=
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate ocp
 
-python -c "import casadi, numpy as np; print('casadi', casadi.__version__, 'numpy', n>
+python -c "import casadi, numpy as np; print('casadi', casadi.__version__, 'numpy', np.__version__)"
+
 
 python /pfs/data6/home/fr/fr_fr/fr_rl1038/awebox/examples/Leuthold_2025_RLL_paper_scripts/with_ineq_at_all_but_integral_and_tf_fraction/verification_dynamic_conditions_from_haas2019_test.py
--- examples/Leuthold_2025_RLL_paper_scripts/with_ineq_at_all_but_integral_and_tf_fraction/veri_rot_test.sh
+++ examples/Leuthold_2025_RLL_paper_scripts/with_ineq_at_all_but_integral_and_tf_fraction/veri_rot_test.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=48
-#SBATCH --mem=50G
+#SBATCH --mem=32G
 #SBATCH --output=veri_rot_test.out
 #SBATCH --error=veri_rot_test.err
 
@@ -20,7 +20,7 @@ module load devel/miniforge   # bwHPC module :contentReference[oaicite:1]{index=
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate ocp
 
-python -c "import casadi, numpy as np; print('casadi', casadi.__version__, 'numpy', n>
+python -c "import casadi, numpy as np; print('casadi', casadi.__version__, 'numpy', np.__version__)"
 
 python /pfs/data6/home/fr/fr_fr/fr_rl1038/awebox/examples/Leuthold_2025_RLL_paper_scripts/with_ineq_at_all_but_integral_and_tf_fraction/verification_rotationally_steady_from_haas2017_test.py
 
