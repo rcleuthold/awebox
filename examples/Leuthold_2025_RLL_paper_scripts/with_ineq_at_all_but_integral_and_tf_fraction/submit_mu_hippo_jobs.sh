cee06486018721d41d8d4ba4076ef13adc199f90
RCLeuthold <rcleuthold@gmail.com>
2026-01-15 19:12:08 +0100

    [UNTESTED] filename fix

--- examples/Leuthold_2025_RLL_paper_scripts/with_ineq_at_all_but_integral_and_tf_fraction/submit_mu_hippo_jobs.sh
+++ examples/Leuthold_2025_RLL_paper_scripts/with_ineq_at_all_but_integral_and_tf_fraction/submit_mu_hippo_jobs.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 BASE_DIR="/home/fr/fr_fr/fr_rl1038/awebox/examples/Leuthold_2025_RLL_paper_scripts/with_ineq_at_all_but_integral_and_tf_fraction"
-SBATCH_SCRIPT="${BASE_DIR}/run_one_mu_hippo.sbatch"
+SBATCH_SCRIPT="${BASE_DIR}/run_mu_hippo_array.sbatch"
 
 MU_VALUES=(1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7)
 
